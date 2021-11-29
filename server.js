0f4a274ae901303a9fe4c9449e90b51e3da46ad2
Jas <ragnarock.josef@gmail.com>
2021-11-29 15:55:38 -0500

    added views to server.js

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
