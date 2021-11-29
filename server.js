3e64217b77ce99cc4e7251edda1ba2341c0b777a
Jas <ragnarock.josef@gmail.com>
2021-11-29 16:11:09 -0500

    changed server.js

--- server.js
+++ server.js
@@ -33,6 +33,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 
+app.get('/', async (req, res) => {
+  try {
+      const postData = await Post.findAll({
+          include: [
+              {
+                  model: User,
+                  attributes: ['name'],
+              },
+          ],
+      });
+
+      const posts = postData.map((post) => post.get({ plain: true }));
+
+      // Pass serialized data and session flag into template
+      res.render('homepage', {
+          posts,
+          logged_in: req.session.logged_in
+      });
+  } catch (err) {
+      res.status(500).json(err);
+  }
+});
+
+
+// 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
